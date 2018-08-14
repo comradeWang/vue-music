51f086a8856b48a7c05e1dfcb0f6869dd9851b25
WangXinYu <615948603@qq.com>
2018-08-14 14:45:24 +0800

    壮志饥餐胡虏肉,笑谈渴饮匈奴血。
    （增加了scroll组件，基于better-scroll开发）

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper" v-if="sliderList.length">
-        <div class="swiper-container">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide" v-for="(item,$index) in sliderList" :key="$index">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl" :alt="item.id" width="100%" height="200px">
-              </a>
+    <Scroll class="recommend-content" :data="discList">
+      <div class="content">
+        <div class="slider-wrapper" v-if="sliderList.length">
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" v-for="(item,$index) in sliderList" :key="$index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl" :alt="item.id" width="100%" height="200px">
+                </a>
+              </div>
             </div>
-            <!--<div class="swiper-slide"><img :src="sliderList[0].picUrl" :alt="sliderList[0].id" width="100%" height="200px"></div>-->
-            <!--<div class="swiper-slide"><img :src="sliderList[1].picUrl" :alt="sliderList[1].id" width="100%" height="200px"></div>-->
+            <div class="swiper-pagination"></div>
           </div>
-          <div class="swiper-pagination"></div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="(item,index) in discList" :key="index">
+              <div class="disc-container">
+                <div class="icon">
+                  <img :src="item.imgurl" alt="" width="60" height="60">
+                </div>
+                <div class="text">
+                  <h2 class="name"> {{item.creator.name}}</h2>
+                  <p class="desc"> {{item.dissname}}</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="loading-container">
+          <Loading :show="!discList.length"></Loading>
         </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
-      </div>
-    </div>
-    <div class="loading-container">
-      <Loading :show="!descList.length"></Loading>
-    </div>
+    </Scroll>
   </div>
 </template>
 
@@ -31,30 +43,24 @@ import Loading from '../../base/loading/loading'
 import { getRecommend, getDiscList } from '../../api/recommend'
 import { ERR_OK } from '../../api/config'
 import Slider from '../../base/slider/slider'
-
 import Swiper from 'swiper'
 import 'swiper/dist/css/swiper.min.css'
+import Scroll from '../../base/scroll/scroll'
 export default {
   name: 'recommend',
   components: {
     Loading,
-    Slider
+    Slider,
+    Scroll
   },
   data () {
     return {
-      descList: [],
+      discList: [],
       sliderList: [],
       mySwiper: ''
     }
   },
   methods: {
-    _getRecommend () {
-      getRecommend().then((res) => {
-        if (res.code === ERR_OK) {
-          this.sliderList = res.data.slider
-        }
-      })
-    },
     initSwiper () {
       this.mySwiper = new Swiper('.swiper-container', {
         autoplay: {
@@ -69,12 +75,21 @@ export default {
         }
       })
     },
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.sliderList = res.data.slider
+        }
+      })
+    },
+    /**
+     * 获取歌单信息
+     * @private
+     */
     _getDiscList () {
       getDiscList().then((res) => {
-        // let result = this.musicJsonCallback(res)
-        console.log(res)
         if (res.code === ERR_OK) {
-          console.log(res.data)
+          this.discList = res.data.list
         }
       })
     }
@@ -95,12 +110,49 @@ export default {
 <style scoped lang="scss">
   @import "../../common/scss/variable";
   .recommend {
-    my-bullet-active
     .loading-container {
       position: absolute;
       width: 100%;
       top: 50%;
       transform: translateY(-50%);
+    }
+    .recommend-content{
+      height: 100%;
+      overflow: hidden;
+      .recommend-list {
+        .list-title {
+          color: $color-theme;
+          padding: 10px 20px;
+          font-size: $font-size-16;
+        }
+        .disc-container {
+          display: flex;
+          padding: 10px 20px;
+          align-items: center;
+          box-sizing: border-box;
+          position: relative;
+          .icon {
+            flex: 0 0 60px;
+          }
+          .text {
+            flex: 1;
+            text-align: left;
+            padding-left: 10px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            line-height: 20px;
+            .name {
+              font-size: 1rem;
+              margin-bottom: 15px;
+            }
+            .desc {
+              font-size: .6rem;
+              color: $color-text-l;
+            }
+          }
+        }
+      }
     }
   }
 </style>
