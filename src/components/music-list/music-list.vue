3f74a6a4c1c9707c6e3087cdf6af6318194766cf
WangXinYu <615948603@qq.com>
2018-08-22 09:11:20 +0800

    春风自绿江南岸，明月何时照我还。

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -6,15 +6,35 @@
       </div>
       <div class="title">{{title}}</div>
     </div>
-    <div class="bg-image" :style="backgroundImg"></div>
+    <div class="bg-image" :style="backgroundImg" ref="bgImage">
+      <div class="filter"></div>
+    </div>
+    <div class="bg-layer" ref="layer"></div>
+    <Scroll
+      :probe-type="probeType"
+      :listen-scroll="listenScroll"
+      :data="songList" class="list"
+      ref="list"
+      @scroll="scroll">
+      <div class="song-list-wrapper">
+        <SongList :songs="songList"></SongList>
+      </div>
+    </Scroll>
   </div>
 </template>
 
 <script>
+import Scroll from '../../base/scroll/scroll'
+import SongList from '../song-list/song-list'
+const REHEIGHT = 40
 export default {
   name: 'music-list',
+  components: {SongList, Scroll},
   data () {
     return {
+      probeType: 3,
+      listenScroll: true,
+      scrollY: 0 // y轴方向滚动的距离
     }
   },
   props: {
@@ -31,7 +51,23 @@ export default {
       default: null
     }
   },
-  watch: {},
+  watch: {
+    scrollY (newY) {
+      let translateY = Math.max(this.minTranlateY, newY)
+      let zIndex = -1
+      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+      this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      if (this.minTranlateY === translateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = '0'
+        this.$refs.bgImage.style.height = '40px'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+    }
+  },
   computed: {
     backgroundImg () {
       return `background-image: url(${this.bgImage})`
@@ -40,38 +76,73 @@ export default {
   methods: {
     backToSinger () {
       this.$router.push('/singer')
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
     }
   },
-  mounted () {}
+  mounted () {
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    this.minTranlateY = -this.imageHeight + REHEIGHT
+  }
 }
 </script>
 
 <style scoped lang="scss">
-@import "~common/scss/variable";
-  .music-list{
+  @import "~common/scss/variable";
+
+  .music-list {
     position: relative;
-    .back-title{
+    height: 100vh;
+    .back-title {
       padding: 10px 15px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
     }
-    .back{
+    .back {
       color: $color-theme;
       font-size: 1.5rem;
       float: left;
     }
-    .title{
+    .title {
       text-align: center;
       font-size: 1.2rem;
     }
-    .bg-image{
+    .bg-image {
       width: 100%;
-      height: 300px;
-      background-size: 100% 100%  ;
+      height: 0;
+      padding-top: 70%;
+      background-size: cover;
       background-position: center;
       background-repeat: no-repeat;
+      position: relative;
+      z-index: -1;
+      .filter {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(7, 17, 27, 0.4);
+      }
+    }
+    .list {
       position: fixed;
       top: 0;
-      left: 0;
-      z-index: -1;
+      bottom: 0;
+      width: 100%;
+      background-color: $color-background;
+      /*overflow: hidden;*/
+      .song-list-wrapper {
+      }
+    }
+    .bg-layer {
+      position: relative;
+      height: 100%;
+      background: #222222;
     }
   }
 </style>
---
+++ src/components/song-list/song-list.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class='song-list'>
+    <ul>
+      <li v-for="(song,$index) in songs" :key="$index" class="item">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'song-list',
+  data () {
+    return {}
+  },
+  props: {
+    songs: {
+      type: Array,
+      default: null
+    }
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  methods: {
+    getDesc (song) {
+      return `${song.singer} - ${song.album}`
+    }
+  },
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~common/scss/variable";
+  .song-list {
+    height: 100%;
+    .item{
+      display: flex;
+      align-items:center;
+      box-sizing: border-box;
+      height: 64px;
+      font-size: $font-size-16;
+      text-align: left;
+      padding: 0 15px;
+    }
+    .content{
+      flex: 1;
+      line-height: 20px;
+      font-size: $font-size-16 !important;
+      overflow: hidden;
+      .name{
+        font-size: $font-size-16 !important;
+      }
+      .desc{
+        color: $color-text-l;
+      }
+    }
+  }
+</style>
